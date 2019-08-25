0ce310efd1e95b7ba9e6d9a5e0053bdd2f2f5fbe
Ananthakumar <ananthakumar.akr@gmail.com>
2019-08-25 09:22:10 +0530

    dark mode added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("👋 Hola!");
+const switcher = document.getElementsByClassName("switch")[0];
+const root = document.getElementsByTagName("html")[0];
+
+// detects if dark mode is available, for default setup
+const isDarkMode =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+root.className = isDarkMode ? "dark" : "light";
+
+switcher.addEventListener("click", () => {
+  root.className = root.className === "light" ? "dark" : "light";
+});
