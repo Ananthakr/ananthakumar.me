341acc0350a4bab0385e2df79816bfc0673606f0
Ananthakumar <ananthakumar.akr>
2021-06-16 15:16:52 +0530

    Web vital refactoring and parcel v2 adoption

---
+++ dist/index.f2221767.js
@@ -0,0 +1,2 @@
+!function(){console.log("👋 Hola!");const e=document.getElementsByClassName("switch")[0],a=document.getElementsByTagName("html")[0],t=window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches;a.className=t?"dark":"light",e.addEventListener("click",(()=>{a.className="light"===a.className?"dark":"light"}))}();
+//# sourceMappingURL=index.f2221767.js.map
